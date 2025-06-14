c4750cd29a9928d08061f3a0182b130c51b5bb50
dsp-mark <dsp.engineer.mark@gmail.com>
2025-06-13 23:13:26 -0700

    Initial commit; super basic

---
+++ README.md
@@ -0,0 +1,4 @@
+# RSX 2025 Iridium Server
+
+Super basic flask server to receive and store data from the RSX 2025 capsule received from the Iridium Satellite Network.
+
---
+++ server/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import sqlite3 as sqlite
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite.connect('rsx.db')
+    c = conn.cursor()
+    c.execute('''
+                CREATE TABLE IF NOT EXISTS raw_data (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                message TEXT NOT NULL  
+                );
+              ''')
+    conn.commit()
+    conn.close()
+
+
+@app.route("/raw_iridium", methods=["POST"])
+def receive_iridium():
+    data = request.get_json()
+    content = data.get("message")
+
+    if not content:
+        return jsonify({'error': 'No message provided'}), 400
+    
+    conn = sqlite.connect('rsx.db')
+    c = conn.cursor()
+    c.execute('''
+              INSERT INTO raw_data (content) VALUES (?)
+              ''', (content,))
+    conn.commit()
+    conn.close()
+    return jsonify({'status': 'Message stored'}), 200
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
